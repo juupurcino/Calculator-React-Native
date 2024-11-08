46329629ed7198e14ad8b36ee4547ab28020acd7
juupurcino <jdiaspurcino@gmail.com>
2024-11-08 16:07:24 -0300

    quase pronta

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,39 @@
-import React from "react";
-import { View, Alert, StyleSheet, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Alert, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 import { Button } from "@/components/button";
+import Svg, { Path } from 'react-native-svg';
 
 export default function HomeScreen() {
-  const handlePress = () => {
-    Alert.alert("Botão pressionado!");
+  
+  const [visor, setvisor] = useState("0");
+  const [equacao, setequacao] = useState("0");
+
+  const mudarBotoes = () => {
+
+  }
+  
+  const handlePress = (buttonText: React.SetStateAction<string>, sinais: React.SetStateAction<string>) => {
+    if (buttonText === "AC") {
+      setvisor("0"); 
+      setequacao("0"); 
+      
+    } else if (buttonText === "=") {
+      try {
+        setvisor(eval(equacao).toString()); 
+      } catch (error) {
+        setvisor("Erro"); 
+      }
+    } else if (visor === "0" && buttonText !== ",") {
+      
+      setequacao(buttonText);
+      setvisor(sinais);
+
+    }else if (buttonText == "%") {
+      
+    } else {
+      setvisor((prev) => prev +  sinais); 
+      setequacao((prev) => prev + buttonText); 
+    }
   };
 
   return (
@@ -12,41 +41,43 @@ export default function HomeScreen() {
       <View style={styles.fundo}>
         <View style={styles.cima}>
           <View style={styles.visor}>
-            <Text style={styles.textVisor}>123</Text>
+            <Text style={styles.textVisor}>{visor}</Text>
           </View>
         </View>
 
         <View style={styles.botoes}>
           <View style={styles.linhas}>
-            <Button text="AC" colorText="#f7df9c" colorBack="#5F483AFF" colorButton="#876D5DFF" press={handlePress}/>
-            <Button text="±" colorText="#f7df9c" colorBack="#5F483AFF" colorButton="#876D5DFF" press={handlePress}/>
-            <Button text="%" colorText="#f7df9c" colorBack="#5F483AFF" colorButton="#876D5DFF"press={handlePress}/>
-            <Button text="÷" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={handlePress}/>
+            <Button text="AC" colorText="#f7df9c" colorBack="#5F483AFF" colorButton="#876D5DFF" press={() => handlePress("AC", "AC")}/>
+            <Button text="±" colorText="#f7df9c" colorBack="#5F483AFF" colorButton="#876D5DFF" press={() => handlePress("±", "±")}/>
+            <Button text="%" colorText="#f7df9c" colorBack="#5F483AFF" colorButton="#876D5DFF" press={() => handlePress("%", "%")}/>
+            <Button text="÷" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={() => handlePress("/", "÷")}/>
           </View>
           <View style={styles.linhas}>
-            <Button text="9" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="8" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="7" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="–" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={handlePress}/>
+            <Button text="9" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("9", "9")}/>
+            <Button text="8" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("8", "8")}/>
+            <Button text="7" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("7", "7")}/>
+            <Button text="–" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={() => handlePress("-", "-")}/>
           </View>
           <View style={styles.linhas}>
-            <Button text="6" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="5" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="4" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="×" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={handlePress}/>
+            <Button text="6" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("6", "6")}/>
+            <Button text="5" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("5", "5")}/>
+            <Button text="4" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("4", "4")}/>
+            <Button text="×" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={() => handlePress("*", "x")}/>
           </View>
           <View style={styles.linhas}>
-            <Button text="3" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="2" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="1" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="+" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={handlePress}/>
+            <Button text="3" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("3", "3")}/>
+            <Button text="2" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("2", "2")}/>
+            <Button text="1" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("1", "1")}/>
+            <Button text="+" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={() => handlePress("+", "+")}/>
           </View>
           <View style={styles.linhas}>
-            <TouchableOpacity style={styles.zero}>
-              <Text style={styles.textZero}>0</Text>
+            
+            <TouchableOpacity style={styles.buttonIcon} onPress={() => mudarBotoes()}>
+              <Image source={require("../assets/images/calculator-svgrepo-com.svg")} style={styles.icon}/>
             </TouchableOpacity>
-            <Button text="," colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={handlePress}/>
-            <Button text="=" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={handlePress}/>
+            <Button text="." colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress(".", ".")}/>
+            <Button text="0" colorText="#f7df9c" colorBack="#f76b6a" colorButton="#D64949FF" press={() => handlePress("0", "0")}/>
+            <Button text="=" colorText="#f76b6a" colorBack="#EEC64EFF" colorButton="#876D5DFF" press={() => handlePress("=", "=")}/>
           </View>
         </View>
       </View>
@@ -77,7 +108,9 @@ const styles = StyleSheet.create({
 
     fontSize: 80,
     fontWeight: 800,
-    color: "#5F483AFF"
+    color: "#5F483AFF",
+    maxWidth: "100%"
+
 
   },
 
@@ -142,6 +175,28 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 800,
     color: "#f7df9c"
+  },
+
+  icon:{
+
+    width: "100%",
+    flex: 1
+  },
+
+  buttonIcon: {
+    backgroundColor: "#f76b6a", 
+    padding: 10,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width:"19%",
+    margin: 7,
+    borderWidth: 8,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomColor: "#D64949FF"
+
   }
 
 
